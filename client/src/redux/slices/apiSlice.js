df81159b9f777a539d7e0f17a502470029751394
sobran5883 <sobran0621@gmail.com>
2025-05-23 04:07:13 +0530

    deployment issue fixed

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
-const API_URI = "http://localhost:5000"
+// const API_URI = "http://localhost:5000"
+const API_URI = ""
 
 const baseQuery = fetchBaseQuery({ 
     baseUrl: API_URI + "/api",
