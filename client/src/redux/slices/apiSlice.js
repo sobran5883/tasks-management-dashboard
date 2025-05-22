9dd262ada91b4accff718e09b10c5a7a60be3eb8
sobran5883 <sobran0621@gmail.com>
2025-05-23 04:28:24 +0530

    production issue fixed

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 // const API_URI = "http://localhost:5000"
-const API_URI = ""
+const API_URI = "https://tasks-management-dashboard.onrender.com"
 
 const baseQuery = fetchBaseQuery({ 
     baseUrl: API_URI + "/api",
